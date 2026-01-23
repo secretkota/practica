05a37d5e2a3416512319ec44fc5c880518318cce
secretkota <stasbro123@gmail.com>
2026-01-23 17:50:14 +0200

    upd: update text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
         <div class="w-full max-w-5xl mx-auto p-4 md:py-8 ">
             <div class="sm:flex sm:items-center sm:justify-between">
                 <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                    <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap mr-2">Flowbite</span>
+                    <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap mr-2">Bank</span>
                 </a>
                 <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-body sm:mb-0">
                     <li>
@@ -25,8 +25,8 @@
                 </ul>
             </div>
             <hr class="my-6 border-default sm:mx-auto lg:my-8" />
-            <span class="block text-sm text-body sm:text-center">© 2023 <a href="https://flowbite.com/"
-                    class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+            <span class="block text-sm text-body sm:text-center">© 2026 <a href="https://flowbite.com/"
+                    class="hover:underline">Bank</a>. All Rights Reserved.</span>
         </div>
     </footer>
 </template>
--- src/components/Header/sidebar.vue
+++ src/components/Header/sidebar.vue
@@ -3,9 +3,9 @@
         class="fixed top-0 left-0 z-40 w-44 h-full transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar">
         <div class="h-full px-3 py-4 overflow-y-auto bg-neutral-primary-soft border-e border-default">
-            <a href="https://flowbite.com/" class="flex items-center ps-2.5 mb-5">
+            <a href="/" class="flex items-center ps-2.5 mb-5">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 me-3" alt="Flowbite Logo" />
-                <span class="self-center text-lg text-heading font-semibold whitespace-nowrap">Flowbite</span>
+                <span class="self-center text-lg text-heading font-semibold whitespace-nowrap">Bank-Test</span>
             </a>
             <ul class="space-y-2 font-medium">
                 <li>
